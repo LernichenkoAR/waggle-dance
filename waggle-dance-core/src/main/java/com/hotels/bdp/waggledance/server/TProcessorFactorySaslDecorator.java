106964735cd1f5f3ebf2574d1794d7878966662d
alernichenko <lernichenko.a.r@omega.sbrf.ru>
2019-09-04 15:26:16 +0300

    TProcessor without Proxy (CSW-195)

--- waggle-dance-core/src/main/java/com/hotels/bdp/waggledance/server/TProcessorFactorySaslDecorator.java
+++ waggle-dance-core/src/main/java/com/hotels/bdp/waggledance/server/TProcessorFactorySaslDecorator.java
@@ -43,7 +43,7 @@ public class TProcessorFactorySaslDecorator extends TProcessorFactory {
   public TProcessor getProcessor(TTransport transport) {
     try {
       TProcessor tProcessor = tProcessorFactory.getProcessor(transport);
-      return saslServer.wrapProcessor(tProcessor);
+      return saslServer.wrapNonAssumingProcessor(tProcessor);
     } catch (RuntimeException e) {
       throw new RuntimeException("Error creating SASL wrapped TProcessor", e);
     }
